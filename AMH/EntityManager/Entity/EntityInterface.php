4eceb0531cef690b0ef20d52be1db2297297dccf
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-07 11:12:43 +0200

    parent interface and classes for all entities

---
+++ AMH/EntityManager/Entity/EntityInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace AMH\EntityManager\Entity;
+
+/**
+@author Alex Horkun mindkilleralexs@gmail.com
+
+Entity carcas.
+*/
+interface EntityInterface{
+	/**
+	@return int Database ID.
+	*/
+	public function id();
+}
+?>
