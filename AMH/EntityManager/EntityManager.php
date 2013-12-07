e2dbdda0ab1e9f87d82d943c6d44a3e59db7d5eb
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-07 09:28:30 +0200

    properties; constructor, setRepositories and setDefaultCache methods

--- AMH/EntityManager/EntityManager.php
+++ AMH/EntityManager/EntityManager.php
@@ -1,12 +1,55 @@
 <?php
 namespace AMH\EntityManager;
 
+use Cache\CacheInterface;
+use Repository\Repository;
+
 /**
 @author Alex Horkun mindkilleralexs@gmail.com
 
 Main interfacce to work with entities.
 */
 class EntityManager{
-
+	/**
+	@var array of repositories.
+	*/
+	private $repos=array();
+	/**
+	@var Cache\CacheInterface Default cache that will be given to all new repos if they doesn't have one.
+	*/
+	private $default_cache=NULL;
+	/**
+	@param array of Repository.
+	@throws \InvalidArgumentException if array given as first argument contains something but repositories.
+	*/
+	public function __construct(array $repos=array(), Cache\CacheInterface $cache=NULL){
+		if($repos){
+			$this->setRepositories($repos);
+		}
+		
+		if($cache){
+			$this->setDefaultCache($cache);
+		}
+	}
+	/**
+	Sets default Cache that will be given to all new repos.
+	*/
+	public function setDefaultCache(CacheInterface $cache){
+		$this->default_cache=$cache;
+	}
+	/**
+	Sets repositories.
+	@throws \InvalidArgumentException if given array contains something but repositories.
+	*/
+	public function setRepositories(array $repos){
+		foreach($repos as $rep){
+			if(!($rep instanceof Repository)){
+				throw new \InvalidArgumentException(get_class($this).' constractor expects first argument to be an array of AMH\Repository\Repository');
+				return;
+			}
+		}
+		$this->repos=$repos;
+		unset($rep,$repos);
+	}
 }
 ?>
