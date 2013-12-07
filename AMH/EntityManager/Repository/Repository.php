146eee8dbd2eb8f8e41ba29ef47b5b83e3e00b07
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-07 11:11:58 +0200

    properties

--- AMH/EntityManager/Repository/Repository.php
+++ AMH/EntityManager/Repository/Repository.php
@@ -1,12 +1,39 @@
 <?php
 namespace AMH\EntityManager\Repository;
 
+use \AMH\EntityManager\Entity\EntityInterface;
+use Mapper\MapperInterface;
+use \AMH\EntityManager\Cache\CacheInterface;
+
 /**
 @author Alex horkun mindkilleralexs@gmail.com
 
 Repository is used to work with Mapper and to store entitties in memory and cache.
 */
 class Repository{
-
+	/**
+	@var array of Entities and flush action information.
+	*/
+	private $entities=array();
+	/**
+	@var string
+	*/
+	private $name=NULL;
+	/**
+	@var MapperInterface
+	*/
+	private $mapper=NULL;
+	/**
+	@var CacheInterface
+	*/
+	private $cache=NULL;
+	/**
+	@var HydaratorInterface to fetch entity from array/extract entity to array.
+	*/
+	private $hydarator=NULL;
+	/**
+	@var EntityManager
+	*/
+	private $em=NULL;
 }
 ?>
