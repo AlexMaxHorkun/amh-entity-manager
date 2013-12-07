88eaa2aa838fcedcc3172055284e4bc4f8bec889
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-07 10:10:04 +0200

    repository

---
+++ AMH/EntityManager/Repository/Repository.php
@@ -0,0 +1,12 @@
+<?php
+namespace AMH\EntityManager\Repository;
+
+/**
+@author Alex horkun mindkilleralexs@gmail.com
+
+Repository is used to work with Mapper and to store entitties in memory and cache.
+*/
+class Repository{
+
+}
+?>
