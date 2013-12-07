6d92f44b522866f139d31b781c9b38ecad657c01
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-07 10:09:50 +0200

    cache

---
+++ AMH/EntityManager/Cache/CacheInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace AMH\EntityManager\Cache;
+
+/**
+@author Alex Horkun mindkilleralexs@gmail.com
+
+Interface for cache which Repositories will be using.
+*/
+interface CacheInterface{
+
+}
+?>
