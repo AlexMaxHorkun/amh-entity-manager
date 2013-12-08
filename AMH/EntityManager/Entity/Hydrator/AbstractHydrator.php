79ddf294cd42fc6f95ae8486b7508f9e1d9f1ddc
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-08 17:55:19 +0200

    hydrator

---
+++ AMH/EntityManager/Entity/Hydrator/AbstractHydrator.php
@@ -0,0 +1,12 @@
+<?php
+namespace AMH\EntityManager\Entity\Hydrator;
+
+/**
+@author Alex Horkun mindkilleralexs@gmail.com
+
+Class to create entities from array, set prop from array, extract to array.
+*/
+abstract class AbstractHydrator{
+
+}
+?>
