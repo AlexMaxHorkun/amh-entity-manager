2378274efda46fa46adbf8417136204c298566ca
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-08 16:37:40 +0200

    extends mapper

--- AMH/EntityManager/Cache/CacheInterface.php
+++ AMH/EntityManager/Cache/CacheInterface.php
@@ -1,12 +1,14 @@
 <?php
 namespace AMH\EntityManager\Cache;
 
+use AMH\EntityManager\Repository\Mapper\MapperInterface;
+
 /**
 @author Alex Horkun mindkilleralexs@gmail.com
 
 Interface for cache which Repositories will be using.
 */
-interface CacheInterface{
+interface CacheInterface extends MapperInterface{
 
 }
 ?>
