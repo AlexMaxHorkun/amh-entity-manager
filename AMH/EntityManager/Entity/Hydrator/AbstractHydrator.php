24cd00536bba2d2928a9d396817e227bec7cc7b7
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-10 19:51:06 +0200

    basic hydrator class

--- AMH/EntityManager/Entity/Hydrator/AbstractHydrator.php
+++ AMH/EntityManager/Entity/Hydrator/AbstractHydrator.php
@@ -1,12 +1,83 @@
 <?php
 namespace AMH\EntityManager\Entity\Hydrator;
 
+use AMH\EntityManager\Repository\Repository;
+use AMH\EntityManager\Entity\AbstractEntity as Entity;
+
 /**
 @author Alex Horkun mindkilleralexs@gmail.com
 
 Class to create entities from array, set prop from array, extract to array.
 */
 abstract class AbstractHydrator{
-
+	/**
+	@var Repository
+	*/
+	private $repo=NULL;
+	
+	public function __construct(Repository $repo=NULL){
+		if($repo){
+			$this->setRepository($repo);
+		}
+	}
+	
+	public function setRepository(Repository $repo){
+		$repo->setHydrator($this);
+		$this->repo=$repo;
+	}
+	/**
+	Gets relative entity.
+	
+	@param string Entity classname (repo name).
+	@param int ID of needed entity.
+	
+	@return Entity.
+	*/
+	protected function relative($name, $id){
+		if($em=$this->repo->getEntityManager()){
+			return $em->find($name,$id);
+		}
+		else{
+			throw new \RuntimeException('Cannot find relative entity without link to Entitymanager');
+		}
+	}
+	/**
+	Gets collection of relative entities.
+	
+	@param string Entity classname (repo name).
+	@param array IDs.
+	*/
+	protected function relatives($name, array $ids){
+		if(($em=$this->repo->getEntityManager()) && ($repo=$em->getRepository($name))){
+			return $repo->findByIds($ids);
+		}
+		else{
+			throw new \RuntimeException('Cannot find relative entity without link to Entitymanager');
+		}
+	}
+	/**
+	Creates Entity from array containing id and relative entities' ids.
+	
+	@param array Entity data.
+	
+	@return Entity
+	*/
+	abstract public function createFrom(array $data);
+	/**
+	Fetches entity data (fields which are not ID or relative entities' IDs).
+	
+	@param Entity
+	
+	@return void
+	*/
+	abstract public function fetchEntity(Entity $e);
+	/**
+	Extracts entity to array, relative entities must be return as ids not objects.
+	
+	@param Entity
+	
+	@return array
+	*/
+	abstract public function extract(Entity $e);
 }
 ?>
