10ba06def9c4f910deb35f3f6862c1afe3c6c5bb
AlexMaxHorkun <mindkilleralexs@gmail.com>
2013-12-10 19:25:49 +0200

    now its a class

---
+++ AMH/EntityManager/Cache/AbstractCache.php
@@ -0,0 +1,14 @@
+<?php
+namespace AMH\EntityManager\Cache;
+
+use AMH\EntityManager\Repository\Mapper\AbstractMapper as Mapper;
+
+/**
+@author Alex Horkun mindkilleralexs@gmail.com
+
+Class for cache which Repositories will be using.
+*/
+abstract class AbstractCache extends Mapper{
+
+}
+?>
